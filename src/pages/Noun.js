fa3b8e404571d76e79c200f4a584761965ab8f18
EnesHarman <enesharman60@gmail.com>
2023-07-08 13:24:51 +0300

    Add main menu button to names page

--- src/pages/Noun.js
+++ src/pages/Noun.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Noun.css"
 import { AiOutlineEyeInvisible, AiOutlineEye, AiTwotoneEye } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 const Noun = () => {
     return (
         <div className='body'>
@@ -8,7 +9,7 @@ const Noun = () => {
                 der Bruder
             </div>
 
-            <div className='content content_translate content_plural' style={{ marginBottom: '50px'}}>
+            <div className='content content_translate content_plural' style={{ marginBottom: '50px' }}>
                 Erkek Kardeş
             </div>
 
@@ -16,6 +17,11 @@ const Noun = () => {
                 <div className='next_button col-7'>Sıradaki</div>
                 <div className='info_button col-3'>
                     <AiOutlineEye></AiOutlineEye>
+                </div>
+            </div>
+            <div className='buttons row' style={{width:'72%', marginTop:'30px'}}>
+                <div className='main-menu' >
+                    <Link className='link' to="/">Ana Menü</Link>
                 </div>
             </div>
         </div>
