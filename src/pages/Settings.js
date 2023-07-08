4e15e9408a17e0f727de2a6fac352a8dac28416c
EnesHarman <enesharman60@gmail.com>
2023-07-08 13:28:20 +0300

    Add temp settings screen

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,18 @@
 
 import React from 'react'
+import "./Settings.css"
+import { Link } from 'react-router-dom';
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div className='body'>
+            <h1>Hazırlanıyor...</h1>
+            <div className='buttons row' style={{width:'72%', marginTop:'30px'}}>
+                <div className='main-menu' >
+                    <Link className='link' to="/">Ana Menü</Link>
+                </div>
+            </div>
+        </div>
   )
 }
 
