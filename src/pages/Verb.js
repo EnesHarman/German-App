e6c4c2419715f04c87d122f9c7c7417bd7cf87b6
EnesHarman <enesharman60@gmail.com>
2023-07-08 13:20:26 +0300

    Finish design of verbs page

--- src/pages/Verb.js
+++ src/pages/Verb.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import "./Verb.css"
+import { Link } from 'react-router-dom';
 
 const Verb = () => {
   return (
-    <div>
-      <h1>Verbs</h1>
-      {/* Add your content for the Verbs page */}
+    <div className='body'>
+      <div className='content content_german' style={{ border: 'none' }}>
+        sein
+      </div>
+      <div className='content content_translate' style={{ marginBottom: '10px', border: 'none' }}>
+        olmak
+      </div>
+
+      <div className='forms'>
+        <div className='row'>
+          <div className='form col-5' style={{ backgroundColor: '#101116', color: 'white' }}>
+            ich
+          </div>
+          <div className='form col-5' style={{ backgroundColor: '#2F3148', color: 'white' }}>
+            du
+          </div>
+        </div>
+        <div className='row'>
+          <div className='form col-5' style={{ backgroundColor: '#3F5576', color: 'white' }}>
+            er/sie/es
+          </div>
+          <div className='form col-5' style={{ backgroundColor: '#587099', color: 'white' }}>
+            wir
+          </div>
+        </div>
+        <div className='row'>
+          <div className='form col-5' style={{ backgroundColor: 'rgb(82 111 153)', color: 'white' }}>
+            ihr
+          </div>
+          <div className='form col-5' style={{ backgroundColor: '#8689AC', color: 'white' }}>
+            sie/Sie
+          </div>
+        </div>
+      </div>
+
+      <div className='buttons row' style={{ margin: '30px 0px 15px 0' }}>
+        <div className='next_button '>Sıradaki</div>
+      </div>
+      <div className='buttons row'>
+        <div className='main-menu'>
+          <Link className='link' to="/">Ana Menü</Link>
+        </div>
+      </div>
     </div>
   );
 };
