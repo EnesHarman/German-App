2d03f5a687496a3881d7bd230a3f09698f16a243
EnesHarman <enesharman60@gmail.com>
2023-07-08 14:32:44 +0300

    Add js codes to name screen

---
+++ src/data/Nouns.js
@@ -0,0 +1,22 @@
+const nouns = [
+    {
+        german: "der Bruder",
+        english: "Brother",
+        turkish: "Erkek Kardeş",
+        plural: "die Burder"
+    },
+    {
+        german: "die Mutter",
+        english: "Mother",
+        turkish: "Anne",
+        plural: "die Mutter"
+    },
+    {
+        german: "das Buch",
+        english: "Book",
+        turkish: "Kitap",
+        plural: "dir Bücher"
+    }
+];
+
+export default nouns;
--- src/pages/Noun.js
+++ src/pages/Noun.js
@@ -1,25 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Noun.css"
 import { AiOutlineEyeInvisible, AiOutlineEye, AiTwotoneEye } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import NounsFile from '../data/Nouns.js';
 const Noun = () => {
+    const [nouns, setNouns] = useState([...NounsFile]);
+    const [currentNoun, setCurrentNoun] = useState({});
+    const [descMode, setDescMode] = useState("plural");
+    useEffect(() => {
+        getRandomName();
+    }, [])
+
+    const test = () => {
+        console.log(currentNoun);
+        console.log(nouns);
+        console.log(NounsFile);
+    }
+
+    const getRandomName = () => {
+        if (nouns.length === 1) {
+            setNouns([...NounsFile])
+        }
+
+        const randomIndex = Math.floor(Math.random() * nouns.length);
+        const selectedName = nouns.splice(randomIndex, 1)[0];
+        selectedName.artikel = checkNameArtikel(selectedName.german);
+        setCurrentNoun(selectedName);
+    }
+
+    const checkNameArtikel = (name) => {
+        return name.slice(0, 3);
+    }
+    
+    const changeDescMode = () => {
+        let mode = descMode === "translate" ? "plural" : "translate";
+        setDescMode(mode);
+    }
+
+
     return (
         <div className='body'>
-            <div className='content-noun content_german'>
-                der Bruder
+            <div className={'content-noun content_german ' + currentNoun.artikel}>
+                {currentNoun.german}
             </div>
 
-            <div className='content-noun content_translate content_plural' style={{ marginBottom: '50px' }}>
-                Erkek Kardeş
-            </div>
+
+            {descMode === "translate" &&
+                <div className='content-noun content_translate' style={{ marginBottom: '50px' }} onClick={() => test()}>
+                    {currentNoun.turkish}
+                </div>
+            }
+
+            {descMode === "plural" &&
+                <div className='content-noun content_plural' style={{ marginBottom: '50px' }} onClick={() => test()}>
+                    {currentNoun.plural}
+                </div>
+            }
 
             <div className='buttons row'>
-                <div className='next_button col-7'>Sıradaki</div>
-                <div className='info_button col-3'>
-                    <AiOutlineEye></AiOutlineEye>
+                <div className='next_button col-7' onClick={() => getRandomName()}>Sıradaki</div>
+                <div className='info_button col-3' onClick={() => changeDescMode()}>
+                    {descMode == "translate" && <AiOutlineEyeInvisible></AiOutlineEyeInvisible>}
+                    {descMode == "plural" && <AiOutlineEye></AiOutlineEye>}
                 </div>
             </div>
-            <div className='buttons row' style={{width:'72%', marginTop:'30px'}}>
+            <div className='buttons row' style={{ width: '72%', marginTop: '30px' }}>
                 <div className='main-menu' >
                     <Link className='link' to="/">Ana Menü</Link>
                 </div>
