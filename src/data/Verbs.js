7fa78880b9796a136fc883c648db4337ad889da3
Enes Harman <enes@Eness-MacBook-Pro.local>
2023-07-13 23:30:08 +0300

    Add new verbs

--- src/data/Verbs.js
+++ src/data/Verbs.js
@@ -1,38 +1,235 @@
 const verbs = [
-    {
-        basic:"werden",
-        ich:"werde",
-        du:"wirst",
-        ersiees:"wird",
-        wir: "werden",
-        ihr:"werdet",
-        sieSie:"werden",
-        turkhish:"elde etmek",
-        english:"to get"
-    },
-    {
-        basic:"haben",
-        ich:"habe",
-        du:"hast",
-        ersiees:"hat",
-        wir: "haben",
-        ihr:"habt",
-        sieSie:"haben",
-        turkhish:"sahip olmak",
-        english:"to have"
-    },
-    {
-        basic:"sein",
-        ich:"bin",
-        du:"bist",
-        ersiees:"ist",
-        wir: "sind",
-        ihr:"seid",
-        sieSie:"sind",
-        turkhish:"olmak",
-        english:"to be"
-    }
-    
+  {
+    basic: "sein",
+    ich: "bin",
+    du: "bist",
+    ersiees: "ist",
+    wir: "sind",
+    ihr: "seid",
+    sieSie: "sind",
+    turkish: "olmak",
+    english: "to be"
+  },
+  {
+    basic: "haben",
+    ich: "habe",
+    du: "hast",
+    ersiees: "hat",
+    wir: "haben",
+    ihr: "habt",
+    sieSie: "haben",
+    turkish: "sahip olmak",
+    english: "to have"
+  },
+  {
+    basic: "werden",
+    ich: "werde",
+    du: "wirst",
+    ersiees: "wird",
+    wir: "werden",
+    ihr: "werdet",
+    sieSie: "werden",
+    turkish: "elde etmek",
+    english: "to get"
+  },
+  {
+    basic: "machen",
+    ich: "mache",
+    du: "machst",
+    ersiees: "macht",
+    wir: "machen",
+    ihr: "macht",
+    sieSie: "machen",
+    turkish: "yapmak",
+    english: "to do"
+  },
+  {
+    basic: "können",
+    ich: "kann",
+    du: "kannst",
+    ersiees: "kann",
+    wir: "können",
+    ihr: "könnt",
+    sieSie: "können",
+    turkish: "yapabilmek",
+    english: "to can"
+  },
+  {
+    basic: "sagen",
+    ich: "sage",
+    du: "sagst",
+    ersiees: "sagt",
+    wir: "sagen",
+    ihr: "sagt",
+    sieSie: "sagen",
+    turkish: "demek",
+    english: "to say"
+  },
+  {
+    basic: "gehen",
+    ich: "gehe",
+    du: "gehst",
+    ersiees: "geht",
+    wir: "gehen",
+    ihr: "geht",
+    sieSie: "gehen",
+    turkish: "gitmek",
+    english: "to go"
+  },
+  {
+    basic: "wissen",
+    ich: "weiß",
+    du: "weißt",
+    ersiees: "weiß",
+    wir: "wissen",
+    ihr: "wisst",
+    sieSie: "wissen",
+    turkish: "bilmek",
+    english: "to know"
+  },
+  {
+    basic: "sehen",
+    ich: "sehe",
+    du: "siehst",
+    ersiees: "sieht",
+    wir: "sehen",
+    ihr: "seht",
+    sieSie: "sehen",
+    turkish: "görmek",
+    english: "to see"
+  },
+  {
+    basic: "kommen",
+    ich: "komme",
+    du: "kommst",
+    ersiees: "kommt",
+    wir: "kommen",
+    ihr: "kommt",
+    sieSie: "kommen",
+    turkish: "gelmek",
+    english: "to come"
+  },
+  {
+    basic: "nehmen",
+    ich: "nehme",
+    du: "nimmst",
+    ersiees: "nimmt",
+    wir: "nehmen",
+    ihr: "nehmt",
+    sieSie: "nehmen",
+    turkish: "almak",
+    english: "to take"
+  },
+  {
+    basic: "geben",
+    ich: "gebe",
+    du: "gibst",
+    ersiees: "gibt",
+    wir: "geben",
+    ihr: "gebt",
+    sieSie: "geben",
+    turkish: "vermek",
+    english: "to give"
+  },
+  {
+    basic: "mögen",
+    ich: "mag",
+    du: "magst",
+    ersiees: "mag",
+    wir: "mögen",
+    ihr: "mögt",
+    sieSie: "mögen",
+    turkish: "sevmek",
+    english: "to like"
+  },
+  {
+    basic: "essen",
+    ich: "esse",
+    du: "isst",
+    ersiees: "isst",
+    wir: "essen",
+    ihr: "esst",
+    sieSie: "essen",
+    turkish: "yemek",
+    english: "to eat"
+  },
+  {
+    basic: "trinken",
+    ich: "trinke",
+    du: "trinkst",
+    ersiees: "trinkt",
+    wir: "trinken",
+    ihr: "trinkt",
+    sieSie: "trinken",
+    turkish: "içmek",
+    english: "to drink"
+  },
+  {
+    basic: "lesen",
+    ich: "lese",
+    du: "liest",
+    ersiees: "liest",
+    wir: "lesen",
+    ihr: "lest",
+    sieSie: "lesen",
+    turkish: "okumak",
+    english: "to read"
+  },
+  {
+    basic: "fahren",
+    ich: "fahre",
+    du: "fährst",
+    ersiees: "fährt",
+    wir: "fahren",
+    ihr: "fahrt",
+    sieSie: "fahren",
+    turkish: "gitmek (araçla)",
+    english: "to drive"
+  },
+  {
+    basic: "sehen",
+    ich: "sehe",
+    du: "siehst",
+    ersiees: "sieht",
+    wir: "sehen",
+    ihr: "seht",
+    sieSie: "sehen",
+    turkish: "görmek",
+    english: "to see"
+  },
+  {
+    basic: "möchten",
+    ich: "möchte",
+    du: "möchtest",
+    ersiees: "möchte",
+    wir: "möchten",
+    ihr: "möchtet",
+    sieSie: "möchten",
+    turkish: "istemek",
+    english: "to want"
+  },
+  {
+    basic: "heißen",
+    ich: "heiße",
+    du: "heißt",
+    ersiees: "heißt",
+    wir: "heißen",
+    ihr: "heißt",
+    sieSie: "heißen",
+    turkish: "adı olmak",
+    english: "to be called"
+  },
+  {
+    basic: "helfen",
+    ich: "helfe",
+    du: "hilfst",
+    ersiees: "hilft",
+    wir: "helfen",
+    ihr: "helft",
+    sieSie: "helfen",
+    turkish: "yardım etmek",
+    english: "to help"
+  }
 ];
 
 export default verbs;
